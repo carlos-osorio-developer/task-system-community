441a2038eaecff27ccbc0a0235dbfbb81b783d8d
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-21 11:21:33 -0500

    Add validation for due_date with I18n internationalization from model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,12 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false}
   validates :due_date, presence: true
-  
+  # disallow past due dates
+  validate :future_due_date
+
+  def future_due_date
+    return if due_date.blank?
+    return if due_date > Date.today
+    errors.add :due_date, I18n.t('task.errrors.invalid_due_date')
+  end
 end
