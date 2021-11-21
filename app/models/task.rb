a622c97901ad1440062f9ca16735f62efafd297a
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-21 11:17:45 -0500

    Add validations for models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :category
+
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false}
+  validates :due_date, presence: true
+  
 end
