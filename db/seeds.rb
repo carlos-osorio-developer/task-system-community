facd8263e087ada2ad11c5404085554612ed7d37
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-22 18:41:06 -0500

    Create seed structure for Users and Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['yo', 'andrea', 'leon', 'andres', 'natalia', 'camilo', 'rusbel', 'johan'].each do |name|
+  user = User.new(email: "#{name}@a.a", password: 'sumadre')
+  user.save
+end
+
+puts 'Users created'
+
+['Personal', 'Work', 'Gardening', 'Learning', 'Projects'].each do |categ|
+  category = Category.new(name: name, description: "These are tasks about #{categ.lowercase} stuff")
+  category.save
+end
+
+puts 'Categories has been created'
