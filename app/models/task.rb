95765db55ade5e5d7c121999acd20d5e09a6be79
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-21 20:18:55 -0500

    Allow task model and controller to have nested dynamic forms

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,15 +2,18 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'  
 
+  has_many :participants
+  has_many :commited_users, through: :participants, source: :user
+
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false}
   validates :due_date, presence: true
   # disallow past due dates
   validate :future_due_date
 
-  has_many :participants
-  has_many :commited_users, through: :participants, source: :user
+  accepts_nested_attributes_for :participants, allow_destroy: true
 
+  
   def future_due_date
     return if due_date.blank?
     return if due_date > Date.today
