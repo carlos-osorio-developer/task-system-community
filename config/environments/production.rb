28289ab9c21f094d2cc4b78842e74d6b80d45ed0
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-24 20:47:48 -0500

    Update production to include host for ActionMailer in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "task_system_community_production"
 
+  config.action_mailer.default_url_options = { :host => 'task-sys-community.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
