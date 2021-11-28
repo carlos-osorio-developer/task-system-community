5b1079c89ea8fc338b00cd706c992c588d1e7cff
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-27 23:50:02 -0500

    Add config and tests for dynamic capybara tests

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
   let(:user) { create :user }
-  before(:each) { sign_in user }
-
-  before do
-    driven_by(:rack_test)
-  end
+  before(:each) { sign_in user }  
   
   describe 'GET/tasks' do
     it 'has the right title' do 
@@ -18,13 +14,25 @@ RSpec.describe "Tasks", type: :system do
   describe 'POST /tasks' do
     #needs a bang because it has to be created before the let is called on line 27
     #otherwise the select will be nil at that point
-    let!(:category) { create :category }    
-    it 'has the right title' do 
+    let!(:category) { create :category }  
+    let!(:participant) { create :user }  
+    it 'creates new task with dinamyc participant button', js: true do 
       visit '/tasks/new'
       fill_in 'task[name]', with: 'Task 1'
       fill_in 'task[description]', with: 'Description 1'
       fill_in 'task[due_date]', with: Date.tomorrow
       select category.name, from: 'task[category_id]'
+
+      click_link 'Agregar un participante'   
+      xpath = '//*[@id="addParticipants"]/div/div[1]'
+      within(:xpath, xpath) do
+        select participant.email, from: 'Usuario'
+        select 'responsible', from: 'Rol'
+      end
+
+      click_button 'Crear Task'
+
+      expect(page).to have_content 'Task was successfully created.'
     end
   end
 end
