24ca98d4bb1f015ca2bc780356978a078d11e730
carlos-osorio-developer <osorio.uis@gmail.com>
2021-11-27 19:57:42 -0500

    TDD/BDD: Create a task and check if it redirects to the show page with the right params

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to render_template(:new)
     end    
   end
+
+  describe "POST /tasks" do
+    let(:category) { create :category }
+    let(:participant) { create :user }
+    let(:params) do
+      {
+        "task"=>{
+          "name"=>"Task name test", 
+          "due_date"=> Date.tomorrow, 
+          "category_id"=> category.id.to_s, 
+          "description"=>"Task description test", 
+          "participants_attributes"=>{
+            "0"=>{
+              "user_id"=> participant.id.to_s, 
+              "role"=> "1", 
+              "_destroy"=> "false"
+            }
+          }          
+        }
+      }
+    end
+
+    it 'creates a task and redirect to show page' do
+      post '/tasks', params: params
+
+      #assigns sirve para evaluar un redirect_to @task
+      expect(response).to redirect_to(assigns(:task))
+      
+      follow_redirect! #sirve para evaluar el contenido de la página a la que se redirigió
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Task was successfully created.')
+
+    end
+  end
 end
